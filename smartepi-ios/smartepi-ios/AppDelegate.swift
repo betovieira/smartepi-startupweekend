e3fe5b4e8c49f163f88720f06c60ff88ce13bf71
Humberto Vieira de Castro <humbertovieira.castro@gmail.com>
2015-08-29 21:55:54 -0300

    Todas as configuracções de bluetooth top!

---
+++ smartepi-ios/smartepi-ios/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  Kit Iot Wearable
+//
+//  Created by Vitor Leal on 4/1/15.
+//  Copyright (c) 2015 Telefonica VIVO. All rights reserved.
+//
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Sound |
+            UIUserNotificationType.Alert | UIUserNotificationType.Badge, categories: nil))
+        
+        
+        
+        return true
+    }
+    
+    func applicationWillResignActive(application: UIApplication) {
+    }
+    
+    func applicationDidEnterBackground(application: UIApplication) {
+    }
+    
+    func applicationWillEnterForeground(application: UIApplication) {
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+    }
+    
+    func applicationWillTerminate(application: UIApplication) {
+    }
+}
+
